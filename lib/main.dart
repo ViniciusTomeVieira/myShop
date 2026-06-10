095c5f8d955dffce7d55cdf1633f3750ec868fc4
Vinicius Vieira <vinny.vieira99@hotmail.com>
2026-06-10 18:02:32 -0300

    Creating the cart page with total amount

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/models/cart.dart';
 import 'package:shop/models/product_list.dart';
+import 'package:shop/pages/cart_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -40,7 +41,10 @@ class MyApp extends StatelessWidget {
         ),
         home: ProductsOverviewPage(),
         debugShowCheckedModeBanner: false,
-        routes: {AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailPage()},
+        routes: {
+          AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailPage(),
+          AppRoutes.CART: (ctx) => CartPage(),        
+        },
       ),
     );
   }
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -2,8 +2,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/components/badge_circle.dart';
 import 'package:shop/components/product_grid.dart';
-import 'package:shop/models/product_list.dart';
+import 'package:shop/models/cart.dart';
+import 'package:shop/utils/app_routes.dart';
 
 enum FilterOptions {
   Favorite,
@@ -48,6 +50,18 @@ class _ProductsOverviewPageState extends State<ProductsOverviewPage> {
                 }
               });
             },
+          ),
+          Consumer<Cart>(
+            builder: (ctx, cart, child) => BadgeCircle(
+              value: cart.itemsCount.toString(),
+              child: child!,
+            ),
+            child: IconButton(
+                onPressed: (){
+                  Navigator.of(context).pushNamed(AppRoutes.CART);
+                }, 
+                icon: Icon(Icons.shopping_cart)
+              ),
           ),
         ],
       ),
